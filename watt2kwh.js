16b9d7786adcb02ed2739d82e0f03491e6e42da1
Paul-Reed <pauldreed@gmail.com>
2019-07-08 22:24:18 +0100

    use hrtime to calc interval

--- watt2kwh.js
+++ watt2kwh.js
@@ -9,70 +9,69 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- **/
-module.exports = function(RED) {
+ **/ module.exports = function(RED) {
     function Watt2kWhNode(config) {
         RED.nodes.createNode(this,config);
 	this.maximum = config.maximum;
         var node = this;
-
             switch(config.format) {
-		case 'j': // Joules
+		case 'j': // joule
+                   this.output=1e-3;
+                    break;
+                case 'kj': // kilojoule
                    this.output=1;
                     break;
                 case 'wh': // watthours
+                   this.output=3.6;
+                    break;
+                case 'kwh': // kilowatt-hour
                    this.output=3600;
                     break;
-                case 'kwh': // kilowatthours
-                   this.output=(3600*1000);
-                    break;
-                case 'mwh': // megawatthours
-                    this.output=(1000*1000*3600);
+                case 'mwh': // megawatt-hour
+                    this.output=(1e3*3600);
                     break;
                 default:
-                    // TODO
             }
 
             switch(config.maximumunit) {
                 case "secs":
-                    this.maximum *= 1000;
+                    this.maximum *= 1;
                     break;
                 case "mins":
-                    this.maximum *= 1000 * 60;
+                    this.maximum *= 60;
                     break;
                 case "hours":
-                    this.maximum *= 1000 * 60 * 60;
+                    this.maximum *= 3600;
                     break;
-                default: // "msecs" so no conversion needed
+                default:
             }
-
+		// Overule if left blank or negative values selected
+		if (this.maximum <= 0) {
+		   this.maximum = 3600;
+		   }
 
         node.on('input', function(msg) {
 
-	var time = Math.round((new Date()).getTime());
-	var watts = msg.payload;
-	var lastms = node.lastms||0;
-	var interval = time - lastms;
-
+	var kwatts = (msg.payload / 1000);
+	var hrtime = process.hrtime();
+	var secsnow = ((hrtime[0]) + (hrtime[1] / 1e9));
+	var lastms = node.lastms||secsnow;
+	var interval = secsnow - lastms;
 
 	// Check if input msg is a number, else warn & exit
-	if (isNaN (watts)){
+	if (isNaN (kwatts)){
 		this.error("The input msg is not a number");
 		return;
 		}
+	node.lastms = secsnow;
 
-	node.lastms = time;
-
-//this.error("output multiplier = "+this.output);
-//this.error("maximum = "+this.maximum);
-//this.error("interval = "+interval);
-
-        // Calculate joules from energy - if interval is OK
-        if (interval < this.maximum) {
-            msg.payload = ((interval*watts)/1000)/this.output;
+        // Calculate kilojoules from energy - if interval is OK
+        if ((interval < this.maximum)&&(interval !== 0)) {
+            msg.payload = (interval*kwatts)/this.output;
             node.send(msg);
         } else {
 		return;
