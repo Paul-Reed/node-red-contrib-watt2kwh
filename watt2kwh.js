e28d178af3339f9539025989543153ebe69bfbe8
Paul-Reed <pauldreed@gmail.com>
2019-07-10 22:02:01 +0100

    numerous updates

--- watt2kwh.js
+++ watt2kwh.js
@@ -9,11 +9,14 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
-implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- **/ module.exports = function(RED) {
+ **/
+
+// Many thanks to @bartbutenaers for the learning experience & support!!
+
+module.exports = function(RED) {
     function Watt2kWhNode(config) {
         RED.nodes.createNode(this,config);
 	this.maximum = config.maximum;
